826f1339dadac39398a56911101d3e0c5821a65a
Amine Abbaoui <amine.abbaoui.w@gmail.com>
2026-04-10 10:20:45 +0100

    added a test file 2nd try !!

--- src/main/java/org/xproce/TestClass.java
+++
@@ -1,9 +0,0 @@
-package org.xproce;
-
-public class TestClass {
-  public static void main(String[] args) {
-    DietPlan dietPlan = new DietPlan(2000, 150, 70, 250);
-    System.out.println(dietPlan);
-  }
-  
-}
